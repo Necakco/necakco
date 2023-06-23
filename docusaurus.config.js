63e4c97db51a187cda9420c1a9c65af8d4823962
Necakco <khoavencyti@gmail.com>
2023-06-23 17:16:30 +0700

    fn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://necakco.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/necakco',
+  baseUrl: '/necakco/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
